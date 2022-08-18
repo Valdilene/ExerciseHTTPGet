ab8c0c0937263acaf74bcd409aaf4b5caf9725b1
Valdilene Siqueira <val.siqueira89@gmail.com>
2022-08-18 22:35:35 +0200

    download csv and parse it to float
    Signed-off-by: Valdilene Siqueira <val.siqueira89@gmail.com>

--- src/download.py
+++ src/download.py
@@ -1,13 +1,35 @@
 # import typing elements
 import string
-from typing import Dict
+from typing import Dict, List
 
 # import lib to do HTTP requests
 import requests
 
+def get_dict_from_csv_string(csv_string: string) -> List[Dict]:
+  """function to convert csv string to dict
 
+  Args:
+      csv_string (string): the raw csv
 
-def download_csv_to_dict(url: string) -> Dict:
+  Returns:
+      List[Dict]: list containing the rows encoded in the csv as dicts
+  """
+  rows = csv_string.split('\n')
+  header_raw = rows[0]
+  rows = rows[1:]
+  dict_keys = header_raw.replace(" ", "").split(',')
+  
+  the_table = []
+  for row in rows:
+    values=row.replace(" ", "").split(',')
+    dict_row = {}
+    for key, value in zip(dict_keys, values):
+      dict_row[key] = float(value)
+    the_table.append(dict_row)
+    
+  return the_table
+
+def download_csv_to_dict(url: string) -> List[Dict]:
   """Download a csv from a URL and transform it to dict.
   return the Dict with the data or None if operation was
   not successful
@@ -16,7 +38,8 @@ def download_csv_to_dict(url: string) -> Dict:
       url (string): url where the dict should be found
 
   Returns:
-      Dict: an object containing data from the csv
+    List[Dict]: list containing the rows encoded in the csv as dicts
+
   """
   # This is a HTTP Get call
   response = requests.get(url)
@@ -25,7 +48,14 @@ def download_csv_to_dict(url: string) -> Dict:
     print(f'GET {url} failed with {response.status_code}')
     return None
   
-  print(response.content)
+  content_string = response.content.decode()
+  
+  try:
+    return get_dict_from_csv_string(content_string)
+  except:
+    print(f'dict from {url} could not be converted')
+    return None
+    
   
   
   
--- src/main.py
+++ src/main.py
@@ -2,4 +2,13 @@ from download import download_csv_to_dict
 
 TEST_CSV_URL='https://gist.githubusercontent.com/chriddyp/feaa84b34854e53fb72a/raw/dbba00aeafb981f0f50014030d1b6ad0399d957d/example-data.csv'
 
-download_csv_to_dict(TEST_CSV_URL)
+the_dict_from_remote_url = download_csv_to_dict(TEST_CSV_URL)
+
+if the_dict_from_remote_url is None:
+  exit(1)
+
+index = 0
+for row in the_dict_from_remote_url:
+  print(index, row)
+  print('- '*30)
+  index += 1
